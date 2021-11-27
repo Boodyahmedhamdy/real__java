6a921170661bfb64da486a3f10a7c060515f4aeb
unknown <abdelrahmanahmedhamdy99@gmail.com>
2021-11-27 13:43:34 +0200

    the main source file

---
+++ my_first_GUI/src/Main.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+//        JFrame frame = new JFrame();
+//        frame.setVisible(true);
+//        frame.setSize(606, 600);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setTitle("my first program");
+//
+//        ImageIcon theIcon = new ImageIcon("warning.png");
+//        frame.setIconImage(theIcon.getImage());
+//        frame.getContentPane().setBackground(new Color(86, 17, 180));
+//
+//        new Window();
+//        new Window("one", 33, 22, 48);
+//        new Window("two", 3, 62, 48);
+//        new Window("three", 222, 62, 188);
+        Window window = new Window();
+
+    }
+}
